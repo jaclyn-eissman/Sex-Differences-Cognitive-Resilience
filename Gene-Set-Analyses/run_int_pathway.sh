92fc9bb3466b1486ee802560f15e36a8bd17e9fe
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-16 16:36:15 -0500

    Update run_int_pathway.sh

--- Gene-Set-Analyses/run_int_pathway.sh
+++ Gene-Set-Analyses/run_int_pathway.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#By Jaclyn Eissman, December 7, 2021
 
-##Setting up an array of analyses
+##Setting up a job array of analyses
 int_file_list=$1
 
-##Pathway-level test
+##Perform pathway-level test
 magma --gene-results /data/h_vmac/eissmajm/Sex_Diff_Final/MAGMA/${int_file_list}.genes.raw --set-annot /data/h_vmac/Programs/magma/custom_pathways.txt --out /data/h_vmac/eissmajm/Sex_Diff_Final/MAGMA/${int_file_list}
