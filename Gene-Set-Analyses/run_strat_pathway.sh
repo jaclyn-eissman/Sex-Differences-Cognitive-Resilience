104f6e04dd73b3bbd9211608ac81e678003d55a3
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-16 16:58:30 -0500

    Update run_strat_pathway.sh

--- Gene-Set-Analyses/run_strat_pathway.sh
+++ Gene-Set-Analyses/run_strat_pathway.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+#By Jaclyn Eissman, December 7, 2021
 
-##Setting up an array of analyses
+##Setting up a job array of analyses
 strat_file_list=$1
 
-##Pathway-level test
+##Perform pathway-level test
 magma --gene-results /data/h_vmac/eissmajm/Sex_Diff_Final/MAGMA/${strat_file_list}.genes.raw --set-annot /data/h_vmac/Programs/magma/custom_pathways.txt --out /data/h_vmac/eissmajm/Sex_Diff_Final/MAGMA/${strat_file_list}
 
