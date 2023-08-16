bf516879a92da4aa9cc1efd6fdba4787e24ff63d
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-16 16:16:24 -0500

    Update run_int_gene_test.sh

--- Gene-Set-Analyses/run_int_gene_test.sh
+++ Gene-Set-Analyses/run_int_gene_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#By Jaclyn Eissman, December 7, 2021
 
-##Setting up an array of analyses
+##Setting up a job array for analysis
 int_file_list=$1
 
-##Gene-level test
+##Perform gene-level test
 magma --bfile /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_final --pval /data/h_vmac/eissmajm/Sex_Diff_Final/META/Interaction/${int_file_list}.out use=rs_number,p-value ncol=n_samples --gene-annot /data/h_vmac/eissmajm/Sex_Diff_Final/MAGMA/All_datasets_final.genes.annot --out /data/h_vmac/eissmajm/Sex_Diff_Final/MAGMA/${int_file_list}
