49e9bf1686d68fd4625758b341ab1061fb2349ac
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-16 16:56:07 -0500

    Update run_strat_gene_test.sh

--- Gene-Set-Analyses/run_strat_gene_test.sh
+++ Gene-Set-Analyses/run_strat_gene_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+#By Jaclyn Eissman, December 7, 2021
 
-##Setting up an array of analyses
+##Setting up a job array of analyses
 strat_file_list=$1
 
-##Gene-level test
+##Perform gene-level test
 magma --bfile  /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_final --pval  /data/h_vmac/eissmajm/Sex_Diff_Final/META/SexStrat/${strat_file_list}.out use=rs_number,p-value ncol=n_samples --gene-annot /data/h_vmac/eissmajm/Sex_Diff_Final/MAGMA/All_datasets_final.genes.annot --out /data/h_vmac/eissmajm/Sex_Diff_Final/MAGMA/${strat_file_list}
 
