69f12b1bd64e06238bc64bbf10d137bc85a55960
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-16 17:07:16 -0500

    Add files via upload

---
+++ Genetic-Correlation/gnova_males_globalres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Load correct modules and source virtual environment
+module restore gnova_python2
+source /data/h_vmac/eissmajm/python2/bin/activate
+
+#Set up array of summary stats
+trait_list=$1
+
+#####Sets up a GNOVA job array for all traits 
+/data/h_vmac/eissmajm/GNOVA-2.0/gnova.py /data/h_vmac/eissmajm/SUMSTATS/Munged_Traits/${trait_list}.sumstats.gz /data/h_vmac/eissmajm/Sex_Diff_Final/GNOVA/SexStrat/males/GLOBALRES.males_2sets.sumstats.gz --bfile /data/h_vmac/eissmajm/GNOVA-2.0/genotype_1KG_eur_SNPmaf5/eur_chr@_SNPmaf5 --out /data/h_vmac/eissmajm/Sex_Diff_Final/GNOVA/SexStrat/males/globalres/${trait_list}.males.globalres.txt
