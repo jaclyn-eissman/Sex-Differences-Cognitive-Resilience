2e349c47be613567d7f1537f6615ff51979019fa
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-17 10:16:55 -0500

    Update Zoomed_mem_v_pet.R

--- Build-Figures/Zoomed_mem_v_pet.R
+++ Build-Figures/Zoomed_mem_v_pet.R
@@ -1,11 +1,13 @@
-#packages
+#By Jaclyn Eissman, December 7, 2021
+
+#Load packages
 library(ggplot2)
 library(data.table)
 
-#directory
+#Set directory
 dir <- "/Users/jackieeissman/Box Sync/Hohman_Lab/Students/Jaclyn Eissman/Sex_Diff_Final/"
 
-#read in mem data and organize -- from genetic resilience project update data folder
+#Read in mememory data and organize
 autopsy_mem <- readRDS(paste0(dir,"TABLES/data/ACT_ROSMAP_Combined_Resilience_Phenotype_Covariates_newmem.rds"))
 autopsy_mem <- autopsy_mem[,c("ID","mem","SEX","CERAD","Dataset","dx")]
 names(autopsy_mem) <- c("ID","MEM","SEX","CERAD","Dataset","dx")
@@ -16,7 +18,7 @@ pet_mem <- pet_mem[,c("ID","mem","sex","std_mixture_suvr","Dataset","dx","VISCOD
 names(pet_mem) <- c("ID","MEM","SEX","SUVR","Dataset","dx","VISCODE")
 pet_mem$SEX <- ifelse(pet_mem$SEX==1,"Male", ifelse(pet_mem$SEX==2,"Female",pet_mem$SEX))
 
-#read in final covar file and subset to people present in that
+#Read in final covariate file and subset to people present in that
 covar <- fread(paste0(dir,"DATA/All_datasets_MPlus_Resilience_Covariates_updated.txt"))
 covar$FID <- gsub("A4_","",covar$FID)
 covar$FID <- gsub("ACT_","",covar$FID)
@@ -36,4 +38,3 @@ ggplot(data=pet, aes(x=SUVR, y=MEM, color=SEX)) + geom_point() + geom_smooth(met
   theme(axis.text.y=element_text(colour="black",size=16)) + theme(axis.title.y=element_text(colour="black",size=16,face="bold")) +
   theme(axis.text.x=element_text(colour="black",size=16)) + theme(axis.title.x=element_text(colour="black",size=16,face="bold"))
 dev.off()
-
