6d08994fc9929d64cc915b1a8b132c5ba2458119
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-17 10:08:00 -0500

    Update build_gnova_tables.R

--- Build-Tables/build_gnova_tables.R
+++ Build-Tables/build_gnova_tables.R
@@ -1,11 +1,13 @@
-###packages
+#By Jaclyn Eissman, December 7, 2021
+
+###Load packages
 library(data.table)
 library(openxlsx)
 
-###directory
+###Set directory
 dir <- "/Users/jackieeissman/Box Sync/Hohman_Lab/Students/Jaclyn Eissman/Sex_Diff_Final/"
 
-###read in text files 
+###Read in text files 
 Fcogres <- read.table(paste0(dir,"GNOVA/females/females.cogres.combined.txt"),header=F)
 FcogresNC <- read.table(paste0(dir,"GNOVA/females/females.cogresNC.combined.txt"),header=F)
 Fglobalres <- read.table(paste0(dir,"GNOVA/females/females.globalres.combined.txt"),header=F)
@@ -16,7 +18,7 @@ McogresNC <- read.table(paste0(dir,"GNOVA/males/males.cogresNC.combined.txt"),he
 Mglobalres <- read.table(paste0(dir,"GNOVA/males/males.globalres.combined.txt"),header=F)
 MglobalresNC <- read.table(paste0(dir,"GNOVA/males/males.globalresNC.combined.txt"),header=F)
 
-###pull needed cols
+###Pull needed cols
 Fcogres  <- Fcogres[,c("V17","V3","V4","V6")]
 names(Fcogres) <- c("trait","rho_corrected","se_rho","pvalue_corrected")
 FcogresNC  <- FcogresNC[,c("V17","V3","V4","V6")]
@@ -35,7 +37,7 @@ names(Mglobalres) <- c("trait","rho_corrected","se_rho","pvalue_corrected")
 MglobalresNC  <- MglobalresNC[,c("V17","V3","V4","V6")]
 names(MglobalresNC) <- c("trait","rho_corrected","se_rho","pvalue_corrected")
 
-###order by P value
+###Order by P-value
 Fcogres <- Fcogres[order(Fcogres$pvalue_corrected,decreasing=FALSE),]
 FcogresNC <- FcogresNC[order(FcogresNC$pvalue_corrected,decreasing=FALSE),]
 Fglobalres <- Fglobalres[order(Fglobalres$pvalue_corrected,decreasing=FALSE),]
@@ -46,7 +48,7 @@ McogresNC <- McogresNC[order(McogresNC$pvalue_corrected,decreasing=FALSE),]
 Mglobalres <- Mglobalres[order(Mglobalres$pvalue_corrected,decreasing=FALSE),]
 MglobalresNC <- MglobalresNC[order(MglobalresNC$pvalue_corrected,decreasing=FALSE),]
 
-###do an FDR correction
+###Do an FDR correction
 Fcogres$P.Fdr <- p.adjust(Fcogres$pvalue_corrected, method="fdr")
 FcogresNC$P.Fdr <- p.adjust(FcogresNC$pvalue_corrected, method="fdr")
 Fglobalres$P.Fdr <- p.adjust(Fglobalres$pvalue_corrected, method="fdr")
@@ -57,7 +59,7 @@ McogresNC$P.Fdr <- p.adjust(McogresNC$pvalue_corrected, method="fdr")
 Mglobalres$P.Fdr <- p.adjust(Mglobalres$pvalue_corrected, method="fdr")
 MglobalresNC$P.Fdr <- p.adjust(MglobalresNC$pvalue_corrected, method="fdr")
 
-###rename
+###Rename cols
 names(Fcogres) <- c("Trait","Rho(females)","SE(females)","P(females)","P.FDR(females)")
 names(FcogresNC) <- c("Trait","Rho(females)","SE(females)","P(females)","P.FDR(females)")
 names(Fglobalres) <- c("Trait","Rho(females)","SE(females)","P(females)","P.FDR(females)")
@@ -68,19 +70,19 @@ names(McogresNC) <- c("Trait","Rho(males)","SE(males)","P(males)","P.FDR(males)"
 names(Mglobalres) <- c("Trait","Rho(males)","SE(males)","P(males)","P.FDR(males)")
 names(MglobalresNC) <- c("Trait","Rho(males)","SE(males)","P(males)","P.FDR(males)")
 
-###combine
+###Combine
 cogres <- merge(Fcogres,Mcogres,by="Trait")
 cogresNC <- merge(FcogresNC,McogresNC,by="Trait")
 globalres <- merge(Fglobalres,Mglobalres,by="Trait")
 globalresNC <- merge(FglobalresNC,MglobalresNC,by="Trait")
 
-###order by male p.fdr
+###Order by male p.fdr
 cogres <- cogres[order(cogres$`P.FDR(males)`,decreasing=FALSE),]
 cogresNC <- cogresNC[order(cogresNC$`P.FDR(males)`,decreasing=FALSE),]
 globalres <- globalres[order(globalres$`P.FDR(males)`,decreasing=FALSE),]
 globalresNC <- globalresNC[order(globalresNC$`P.FDR(males)`,decreasing=FALSE),]
 
-###write out to excel sheets
+###Write out to excel sheets
 list_of_datasets <- list("26_COGRES"=cogres,"27_COGRES_NC"=cogresNC,"28_GLOBALRES"=globalres,"29_GLOBALRES_NC"=globalresNC)
 write.xlsx(list_of_datasets,paste0(dir,"TABLES/excel_docs/Supplementary_Tables_26-29.xlsx"))
 
