b801f67d9d0f5e1bb9f6afabe837a1ffbf7e9544
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-17 09:54:04 -0500

    Update run_gcta_reml_all.sh

--- Heritability/run_gcta_reml_all.sh
+++ Heritability/run_gcta_reml_all.sh
@@ -1,13 +1,18 @@
+#By Jaclyn Eissman, December 7, 2021
+
+#Set to fail if there is an error
 set -e  
 
-#create grm and calculate heritability in all
+#Create genetic relatedness matrix (GRM), then calculate heritability with restricted maximum likelihood (REML) method
+
+#In everyone
 gcta64 --bfile /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_final --make-grm --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs --thread-num 10
 gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs --pheno /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt --mpheno 1 --reml --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_herit_COGRES --thread-num 10
 gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs --pheno /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt --mpheno 2 --reml --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_herit_COGRES_NC --thread-num 10
 gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs --pheno /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt --mpheno 3 --reml --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_herit_GLOBALRES --thread-num 10
 gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs --pheno /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt --mpheno 4 --reml --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_herit_GLOBALRES_NC --thread-num 10
 
-#then calculate in males only 
+#In males only
 plink --bfile /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_final --filter-males --make-bed --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_males
 gcta64 --bfile /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_males --make-grm --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_males --thread-num 10
 gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_males --pheno /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt --mpheno 1 --reml --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_herit_COGRES_males --thread-num 10
@@ -15,7 +20,7 @@ gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_ch
 gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_males --pheno /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt --mpheno 3 --reml --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_herit_GLOBALRES_males --thread-num 10
 gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_males --pheno /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt --mpheno 4 --reml --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_herit_GLOBALRES_NC_males --thread-num 10
 
-#then calculate in females only 
+#In females only
 plink --bfile /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_final --filter-females --make-bed --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_females
 gcta64 --bfile /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_females --make-grm --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_females --thread-num 10
 gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_females --pheno /data/h_vmac/eissmajm/Sex_Diff_Final/DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt --mpheno 1 --reml --out /data/h_vmac/eissmajm/Sex_Diff_Final/GCTA/All_datasets_final_all_chrs_herit_COGRES_females --thread-num 10
