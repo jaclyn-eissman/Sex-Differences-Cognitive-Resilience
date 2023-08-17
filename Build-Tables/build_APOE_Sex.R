b780a64cfe4df396a5bf230706bca52f8e9a2390
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-17 10:02:57 -0500

    Update build_APOE_Sex.R

--- Build-Tables/build_APOE_Sex.R
+++ Build-Tables/build_APOE_Sex.R
@@ -1,25 +1,26 @@
+#By Jaclyn Eissman, December 7, 2021
 ###Sensitivity analysis: APOE*SEX
 
-#packages
+#Load packages
 library(data.table)
 library(metafor)
 library(readxl)
 
-#directory
+#Set directory
 directory <- "/Users/jackieeissman/Box Sync/Hohman_Lab/Students/Jaclyn Eissman/Sex_Diff_Final/"
 
-#read in phenotype and covariate files
+#Read in phenotype and covariate files
 covar <- fread(paste0(directory,"DATA/All_datasets_MPlus_Resilience_Covariates_updated.txt"))
 pheno <- fread(paste0(directory,"DATA/All_datasets_MPlus_Resilience_Phenotypes_updated.txt"))
 covar_pheno <- merge(pheno,covar,by=c("FID","IID")) #N=5024
 
-#read in apoe info (data from cohort folders, and ADNI is from 2018 folder)
+#Read in APOE information
 a4 <- read.csv(paste0(directory,"TABLES/data/A4_SUBJINFO.csv"))
 act <- read_excel(paste0(directory,"TABLES/data/ACT_APOE_20210204.xlsx"))
 adni <- readRDS(paste0(directory,"TABLES/data/Apoe.rds"))
 rosmap <- read.csv(paste0(directory,"TABLES/data/dataset_295_basic_05-07-2019.csv"))
 
-#add dataset label to ID and to df
+#Add dataset label to ID and to df
 a4$ID <- paste0("A4_",a4$BID)
 a4$dataset <- "A4"
 act$ID <- paste0("ACT_",act$e165_ID)
@@ -29,7 +30,7 @@ adni$dataset <- "ADNI"
 rosmap$ID <- paste0("ROSMAP_",rosmap$projid)
 rosmap$dataset <- "ROSMAP"
 
-#create apoe4 and apoe2 bins
+#Create apoe4 and apoe2 bins
 a4 <- a4[!is.na(a4$APOEGN),]
 a4$apoe4_bin <- ifelse(a4$APOEGN=="E2/E4" | a4$APOEGN=="E3/E4", 1, ifelse(a4$APOEGN=="E4/E4", 2, 0))
 a4$apoe2_bin <- ifelse(a4$APOEGN=="E2/E4" | a4$APOEGN=="E2/E3" | a4$APOEGN=="E2/E2", 1, 0)
@@ -51,11 +52,11 @@ rosmap$apoe4_bin <- ifelse(rosmap$apoe_genotype=="24" | rosmap$apoe_genotype=="3
 rosmap$apoe2_bin <- ifelse(rosmap$apoe_genotyp=="24" | rosmap$apoe_genotyp=="23" | rosmap$apoe_genotype=="22", 1, 0)
 rosmap <- rosmap[,c("ID","apoe4_bin","apoe2_bin","dataset")]
 
-#combine with covar/pheno file
+#Combine with covariate/phenotype file
 data <- rbind(a4,act,adni,rosmap) #N=14491
 data <- merge(covar_pheno,data,by.x="FID",by.y="ID",all.x=T) #N=5024
 
-###APOE4 
+###APOE4 linear models
 autopsy_cogres <- lm(COGRES ~ age + PC1 + PC2 + PC3 + apoe4_bin*sex, data=data[data$dataset=="ACT"|data$dataset=="ROSMAP",], na.action=na.omit)
 pet_cogres <- lm(COGRES ~ age + PC1 + PC2 + PC3 + apoe4_bin*sex, data=data[data$dataset=="A4"|data$dataset=="ADNI",], na.action=na.omit)
 
@@ -68,7 +69,7 @@ pet_globalres <- lm(GLOBALRES ~ age + PC1 + PC2 + PC3 + apoe4_bin*sex, data=data
 autopsy_globalresNC <- lm(GLOBALRES_NC ~ age + PC1 + PC2 + PC3 + apoe4_bin*sex, data=data[data$dataset=="ACT"|data$dataset=="ROSMAP",], na.action=na.omit)
 pet_globalresNC <- lm(GLOBALRES_NC ~ age + PC1 + PC2 + PC3 + apoe4_bin*sex, data=data[data$dataset=="A4"|data$dataset=="ADNI",], na.action=na.omit)
 
-#meta-analysis
+#Meta-analysis
 cogres <- rma(yi=c(summary(autopsy_cogres)$coefficients["apoe4_bin:sex", "Estimate"], 
               summary(pet_cogres)$coefficients["apoe4_bin:sex", "Estimate"]),
               sei=c(summary(autopsy_cogres)$coefficients["apoe4_bin:sex", "Std. Error"],
@@ -87,7 +88,7 @@ globalresNC <- rma(yi=c(summary(autopsy_globalresNC)$coefficients["apoe4_bin:sex
                        summary(pet_globalresNC)$coefficients["apoe4_bin:sex", "Std. Error"]),method="FE")
 
 
-###APOE2
+###APOE2 linear models
 autopsy_cogres <- lm(COGRES ~ age + PC1 + PC2 + PC3 + apoe2_bin*sex, data=data[data$dataset=="ACT"|data$dataset=="ROSMAP",], na.action=na.omit)
 pet_cogres <- lm(COGRES ~ age + PC1 + PC2 + PC3 + apoe2_bin*sex, data=data[data$dataset=="A4"|data$dataset=="ADNI",], na.action=na.omit)
 
@@ -100,7 +101,7 @@ pet_globalres <- lm(GLOBALRES ~ age + PC1 + PC2 + PC3 + apoe2_bin*sex, data=data
 autopsy_globalresNC <- lm(GLOBALRES_NC ~ age + PC1 + PC2 + PC3 + apoe2_bin*sex, data=data[data$dataset=="ACT"|data$dataset=="ROSMAP",], na.action=na.omit)
 pet_globalresNC <- lm(GLOBALRES_NC ~ age + PC1 + PC2 + PC3 + apoe2_bin*sex, data=data[data$dataset=="A4"|data$dataset=="ADNI",], na.action=na.omit)
 
-#meta-analysis
+#Meta-analysis
 cogres <- rma(yi=c(summary(autopsy_cogres)$coefficients["apoe2_bin:sex", "Estimate"], 
                    summary(pet_cogres)$coefficients["apoe2_bin:sex", "Estimate"]),
               sei=c(summary(autopsy_cogres)$coefficients["apoe2_bin:sex", "Std. Error"],
