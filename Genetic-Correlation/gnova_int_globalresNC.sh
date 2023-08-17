f0c7087d0ebf7dadf49cc2b4a7d3474cb05305d1
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-17 09:38:04 -0500

    Update gnova_int_globalresNC.sh

--- Genetic-Correlation/gnova_int_globalresNC.sh
+++ Genetic-Correlation/gnova_int_globalresNC.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+#By Jaclyn Eissman, December 7, 2021
 
-#Load correct modules and source virtual environment
+#Load module collection and source Python virtual environment
 module restore gnova_python2
 source /data/h_vmac/eissmajm/python2/bin/activate
 
-#Set up array of summary stats
+#Set up job array
 trait_list=$1
 
-#####Sets up a GNOVA job array for all traits 
+#####Runs genetic correlation analysis
 /data/h_vmac/eissmajm/GNOVA-2.0/gnova.py /data/h_vmac/eissmajm/SUMSTATS/Munged_Traits/${trait_list}.sumstats.gz /data/h_vmac/eissmajm/Sex_Diff_Final/GNOVA/Interaction/GLOBALRES_NC.interaction_2sets.sumstats.gz --bfile /data/h_vmac/eissmajm/GNOVA-2.0/genotype_1KG_eur_SNPmaf5/eur_chr@_SNPmaf5 --out /data/h_vmac/eissmajm/Sex_Diff_Final/GNOVA/Interaction/globalresNC/${trait_list}.int.globalresNC.txt 
