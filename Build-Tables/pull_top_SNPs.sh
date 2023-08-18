a22823865ce9006e12285feede907385347c6774
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-18 16:50:05 -0500

    Update pull_top_SNPs.sh

--- Build-Tables/pull_top_SNPs.sh
+++ Build-Tables/pull_top_SNPs.sh
@@ -1,9 +1,11 @@
 #By Jaclyn Eissman, December 7, 2021
 
+#Pull top SNPs from male and female files
 cd /data/h_vmac/eissmajm/Sex_Diff_Final/TABLES
 for i in COGRES COGRES_NC GLOBALRES GLOBALRES_NC; do
 for j in males females; do
 awk 'NR==1 || $10<0.00001 { print $1, $2, $3, $4, $5, $6, $10 }' ../META/SexStrat/${i}.${j}_2sets.out > ${i}.${j}_2sets_TopSNPs.txt; done; done
 
+#Pull top SNPs from interaction files
 for i in COGRES COGRES_NC GLOBALRES GLOBALRES_NC; do
 awk 'NR==1 || $10<0.00001 { print $1, $2, $3, $4, $5, $6, $10 }' ../META/Interaction/${i}.interaction_2sets.out > ${i}.interaction_2sets_TopSNPs.txt; done
